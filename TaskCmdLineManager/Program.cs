9e55325476b7bf74a544c3d7e3e069c6a46d476d
Khaled Zra <halo_434000@hotmail.com>
2022-09-24 11:02:02 +0200

    Added task class

--- TaskCmdLineManager/Program.cs
+++ TaskCmdLineManager/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            if (!(args.Length == 0))
+            {
+                switch (args[0].ToLower())
+                {
+                    case "help":
+                        Help(args);
+                        break;
+
+                    case "init":
+                        Init(args);
+                        break;
+                }
+            }
+        }
+
+        public static void Help(string[] args)
+        {
+            Console.WriteLine("Worked!");
+            Console.WriteLine($"Input: {args[0]}");
+        }
+
+        public static void Init(string[] args)
+        {
+            Console.WriteLine("Worked!");
+            Console.WriteLine($"Input: {args[0]}");
         }
     }
 }
---
+++ TaskCmdLineManager/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskCmdLineManager
+{
+    internal class Task
+    {
+        // Fields
+        private string _taskDescription;
+        private bool _isCompleted;
+
+        // Property
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Default Constructor
+        public Task()
+        {
+            this.TaskDescription = "Default";
+            this.IsCompleted = false;
+        }
+
+        // Constructor
+        public Task(string taskDesc, bool isComp)
+        {
+            this.TaskDescription = taskDesc;
+            this.IsCompleted = isComp;
+        }
+
+        // class Task methods
+        public static Task Initialize(string taskDesc, bool isComp)
+        {
+            return new Task(taskDesc, isComp);
+        }
+    }
+}
